0a78ee1d7f17bf832743cf4e1544128cb999e16a
hariprasanth5474-del <hariprasanth5474@gmail.com>
2026-09-04 20:31:02 +0530

    My first project upload

---
+++ src/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+function Dashboard () {
+    return (
+    <div className="dashboard">
+        <div className="cards">
+            <div className="card">
+                <h3>example card</h3>
+                <p>total customers 400</p>
+            </div>  
+        </div>
+    </div>)
+}
+export default Dashboard
